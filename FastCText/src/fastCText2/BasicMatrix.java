44ac8219c51f2d2ae7e9e80863ee6e2b35d61efa
Vladimir Rodriguez <vladimir.rdguez@gmail.com>
2015-04-30 16:38:44 -0500

    perfeccionando fast CT-ext

---
+++ FastCText/src/fastCText2/BasicMatrix.java
@@ -0,0 +1,46 @@
+package fastCText2;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+import tools.TuplaBinaria;
+
+public class BasicMatrix {
+	TuplaBinaria[] BM;
+	int firstRowOnes, rows;
+
+	public BasicMatrix(TuplaBinaria[] BM, int firstRowOnes, int rows) {
+		this.BM = BM;
+		this.firstRowOnes = firstRowOnes;
+		this.rows = rows;
+	}
+
+//	public int[] getAMlx(int[] AMl, int x) {
+//		return this.BM[x].or(AMl);
+//	}
+//
+//	public int[] getCMlx(int[] AMl, int[] CMl, int x) {
+//		return this.BM[x].getCMlx(CMl, AMl);
+//	}
+
+	public boolean typical(LinkedList<Integer> testor, TuplaBinaria[] acceptanceMasks) {
+		// Get testor's compatibility mask
+		ListIterator<Integer> iterator = testor.listIterator();
+		TuplaBinaria AMl = new TuplaBinaria();
+		TuplaBinaria CMl = new TuplaBinaria();
+		int x;
+		while (iterator.hasNext()) {
+			x = iterator.next();
+			CMl.mascComp(CMl, BM[x], AMl);
+			AMl = acceptanceMasks[x];
+		}
+		// Check that every attribute in testor has a typical row
+		iterator = testor.listIterator();
+		while (iterator.hasNext()) {
+			if (!this.BM[iterator.next()].andNEqZ(CMl)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
---
+++ FastCText/src/fastCText2/FastCText3.java
@@ -0,0 +1,179 @@
+package fastCText2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import tools.TuplaBinaria;
+import fastCText2.BMcolumn;
+
+public class FastCText3 {
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		int typicalTestors = 0, testors = 0, candidates = 0;
+
+		char[][] BMstrRows, BMstrCols;
+
+		// check to see if the String array is empty
+		if (args.length != 1) {
+			System.out.println("The only one argument is the input file name!");
+		}
+
+		String ifilename = args[0];
+
+		System.out.println(ifilename);
+
+		int rows, atts, nwords;
+
+		// Read Input file
+		BufferedReader br = new BufferedReader(new FileReader(ifilename));
+		try {
+			// Size of BM
+			String line = br.readLine();
+			rows = Integer.parseInt(line);
+			line = br.readLine();
+			atts = Integer.parseInt(line);
+
+			// Dual representation for faster processing
+			BMstrRows = new char[rows][atts]; // Representation by rows
+			BMstrCols = new char[atts][rows]; // Representation by columns
+
+			// The rest of the matrix
+			line = br.readLine();
+			int i = 0;
+			String[] temp;
+			while (line != null) {
+				// Parse Row
+				temp = line.trim().replace("\t", " ").split(" ");
+				for (int j = 0; j < atts; j++) {
+					BMstrRows[i][j] = temp[j].charAt(0);
+					BMstrCols[j][i] = temp[j].charAt(0);
+				}
+				i++;
+				line = br.readLine();
+			}
+		} finally {
+			br.close();
+		}
+
+
+		long startTime = System.currentTimeMillis();
+
+		// Sort Basic Matrix
+		BasicMatrix BM = sortBM(BMstrRows, BMstrCols, rows, atts);
+		CandidateGenerator cg = new CandidateGenerator(BM.firstRowOnes, atts);
+		TuplaBinaria[] acceptanceMasks = BM.BM.clone();
+
+		boolean done = false;
+		boolean contributes, testor;
+
+		// Find Testors
+		TuplaBinaria AMl;
+		// BMcolumn AMlx;
+		while (!done) {
+			testor = false; // We don't know yet
+			if (cg.x_1 == -1) {
+				AMl = new TuplaBinaria(rows, -1); // Current candidate has only
+													// 1 attribute
+			} else {
+				AMl = acceptanceMasks[cg.x_1];
+			}
+			// AMlx = new BMcolumn(BM.getAMlx(AMl, cg.x), rows);
+			// acceptanceMasks[cg.x] = AMlx.data;
+			contributes = !acceptanceMasks[cg.x].mascAcep(AMl, BM.BM[cg.x]);
+			if (contributes) {
+				if (acceptanceMasks[cg.x].esUnitario()) {
+					testor = true;
+					testors++;
+					// Check for typical testor
+					if (BM.typical(cg.Current,acceptanceMasks)) {
+						typicalTestors++;
+					}
+				}
+			}
+			done = cg.getCurrentCandidate(testor, contributes);
+		}
+
+		long endTime = System.currentTimeMillis();
+		System.out
+				.println("time: " + Long.toString(endTime - startTime) + "ms");
+		System.out.println("Typical testors: "
+				+ Integer.toString(typicalTestors));
+		System.out.println("Testors: " + Integer.toString(testors));
+		System.out.println("Candidates: " + Integer.toString(candidates));
+	}
+
+	private static BasicMatrix sortBM(char[][] BMstrRows, char[][] BMstrCols,
+			int rows, int atts) {
+		int maxOnes = atts;
+		int j, nOnes = 0;
+		int indx, minRow = 0;
+		TuplaBinaria[] BM = new TuplaBinaria[atts];
+
+		// Find row with minimum ones
+		for (int i = 0; i < rows; i++) {
+			j = 0;
+			nOnes = 0;
+			while ((nOnes < maxOnes) && (j < atts)) {
+				if (BMstrRows[i][j] == '1') {
+					nOnes++;
+				}
+				j++;
+			}
+			if (nOnes < maxOnes) {
+				minRow = i;
+				maxOnes = nOnes;
+			}
+		}
+
+		// Taking index of ones in first row
+		int[] indOnes = new int[nOnes];
+		int[] indZeros = new int[atts - nOnes];
+
+		int o = 0, k = 0;
+
+		for (int i = 0; i < atts; i++) {
+			if (BMstrRows[minRow][i] == '1') {
+				indOnes[o] = i;
+				o++;
+			} else {
+				indZeros[k] = i;
+				k++;
+			}
+		}
+
+		char[][] BMsort = new char[atts][rows]; // By columns
+
+		// Sort Columns
+		for (int i = 0; i < atts; i++) {
+			if (i < nOnes) {
+				indx = indOnes[i];
+			} else {
+				indx = indZeros[i - nOnes];
+			}
+			BMsort[i] = BMstrCols[indx].clone();
+		}
+		// Swap the first rows and the row with minimum number of ones
+		// Save in a BMcolumn array
+		char Temp;
+		for (int i = 0; i < atts; i++) {
+			Temp = BMsort[i][minRow];
+			BMsort[i][minRow] = BMsort[i][0];
+			BMsort[i][0] = Temp;
+			BM[i] = new TuplaBinaria(new String(BMsort[i]));
+		}
+		// // Print sorted BM for testing
+		// for (int r = 0; r < rows; r++) {
+		// for (int i = 0; i < atts; i++) {
+		// System.out.print(BMsort[i][r]);
+		// }
+		// System.out.print('\n');
+		// }
+
+		return new BasicMatrix(BM, nOnes, rows);
+	}
+}
