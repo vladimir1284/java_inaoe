b047defd1967bd3233c9678733e5ce2ada186c1e
Vladimir Rodriguez <vladimir.rdguez@gmail.com>
2015-05-04 18:35:52 -0500

    Starting performance fix

--- FastCText/src/fastCText2/rCText.java
+++ FastCText/src/fastCText2/rCText.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Iterator;
 
+import tools.TPila;
 import tools.TuplaBinaria;
 
 public class rCText {
-	static int typicalTestors = 0, testors = 0, candidates = 0;
+	static int typicalTestors = 0, testors = 0, candidates = 0, cm_last = -1;
 	static BasicMatrix BM;
+	static TPila cm_arr_q = new TPila();
+	static TuplaBinaria[] cm_arr_tup;
 
 	public static void main(String[] args) throws IOException {
 		LinkedList<Integer> B, C;
@@ -28,6 +31,15 @@ public class rCText {
 		long startTime = System.currentTimeMillis();
 		// ---------------------------------------------------------------------------
 		// ***********************_Main_CT-EXT_execution_*****************************
+		// cumulative masks queue
+		int max_cm_depth = BM.atts * (BM.atts - 1) / 2;
+		cm_arr_q.setRange(max_cm_depth);
+		int[][] cm_array = new int[max_cm_depth][TuplaBinaria.numUnidades];
+		cm_arr_tup = new TuplaBinaria[max_cm_depth];
+		for (int i = 0; i < max_cm_depth; i++) {
+			cm_arr_tup[i] = new TuplaBinaria(cm_array[i]);
+		}
+		// ///////////////////////////////////////////////////////////////////
 		int j, i = 0;
 		while (i < BM.firstRowOnes) {
 			candidates++;
@@ -71,34 +83,39 @@ public class rCText {
 		LinkedList<TuplaBinaria> cms = new LinkedList<TuplaBinaria>();
 		Iterator<Integer> iter;
 		int curren_att;
-		
-		iter =  C.iterator();
-		while(iter.hasNext()){
-			cmBx = new TuplaBinaria(BM.rows,-1); //TODO Inefficient
+
+		iter = C.iterator();
+		while (iter.hasNext()) {
+			// cmBx = new TuplaBinaria(BM.rows, -1); // TODO Inefficient
+			cmBx = cm_arr_tup[cm_last+1];
 			curren_att = (int) iter.next();
 			candidates++;
-			if(!cmBx.mascAcep(cmB, BM.BM[curren_att])){
-				if(cmBx.esUnitario()){
+			System.out.println(curren_att);
+			if (!cmBx.mascAcep(cmB, BM.BM[curren_att])) {
+				if (cmBx.esUnitario()) {
 					testors++;
 					// check if typical
 					B.add(curren_att);
-					if(BM.typical(B)){
+					if (BM.typical(B)) {
 						typicalTestors++;
 					}
 					B.removeLast();
-				}else{
-					contrib.add(curren_att);
-					cms.add(cmBx);
+				} else {
+					contrib.addFirst(curren_att);
+					// cms.add(cmBx);
+					cm_last++;// push cm to the queue
 				}
 			}
 		}
-		iter =  contrib.iterator();
-		while(iter.hasNext()){
+		iter = contrib.descendingIterator();
+		while (iter.hasNext()) {
 			curren_att = (int) iter.next();
 			iter.remove();
-			if (iter.hasNext()){ // There are remaining elements in the tail
+			if (iter.hasNext()) { // There are remaining elements in the tail
 				B.add(curren_att);
-				eval(B,cms.removeFirst(),contrib);
+				// eval(B, cms.removeFirst(), contrib);
+				eval(B, cm_arr_tup[cm_last], contrib); 
+				cm_last--;// pop cm
 				B.removeLast();
 			}
 		}
